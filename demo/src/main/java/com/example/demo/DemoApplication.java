39ddc1df39bdd1125b841ec987c30af53ceb8030
vbartalis <vbartalis@gmail.com>
2019-11-09 17:14:52 +0200

    xhtml lapok elkezdve

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import com.sun.faces.config.ConfigureListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.Servlet;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	/*
+	 * Below sets up the Faces Servlet for Spring Boot
+	 */
+	@Bean
+	public FacesServlet facesServlet() {
+		return new FacesServlet();
+	}
+
+	@Bean
+	public ServletRegistrationBean<Servlet> facesServletRegistration() {
+		ServletRegistrationBean<Servlet> registration = new ServletRegistrationBean<Servlet>(facesServlet(), "*.jsf");
+		return registration;
+	}
+
+	@Bean
+	public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+		return new ServletListenerRegistrationBean<ConfigureListener>(new ConfigureListener());
+	}
+}
